7d0f7c776c552fd0c4205e4607dcb6646e85bce9
maks01vac <maks01vac@gmail.com>
2022-07-10 03:18:15 +0300

    button_js_compl

--- buttom_js/js/script.js
+++ buttom_js/js/script.js
@@ -1,6 +1,8 @@
-const button=document.querySelectorAll('.button');
-for(let i=0; i<10; i++){
-    button[i].onclick =function but(){
-    alert('Это кнопка '+(i+1));
-};
+const button = document.querySelectorAll('.button');
+const amount_button = document.getElementsByClassName('button').length
+console.log(amount_button);
+for (let i = 0; i < amount_button; i++) {
+    button[i].onclick = function but() {
+        alert('Это кнопка ' + (i + 1));
+    };
 }
