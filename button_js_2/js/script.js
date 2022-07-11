8e7a588eb0e60a59d598baf838aa34bea6a451ea
maks01vac <maks01vac@gmail.com>
2022-07-11 14:17:07 +0300

    button_ks_2_version_1

---
+++ button_js_2/js/script.js
@@ -0,0 +1,18 @@
+const bodyElement = document.querySelector('body');
+const newDiv = document.createElement('div');
+newDiv.classList.add('main');
+bodyElement.append(newDiv);
+const newButton=[];
+for (let i=0; i<10; i++){
+    newButton[i]=document.createElement('button');
+    newButton[i].classList.add('active');
+    newDiv.append(newButton[i]);
+    newButton[i].append(i+1);
+}
+const amount_button = document.getElementsByClassName('active').length
+console.log(amount_button);
+for (let i = 0; i < amount_button; i++) {
+    newButton[i].onclick = function but() {
+        alert('Это кнопка ' +(i+1));
+    };
+}
