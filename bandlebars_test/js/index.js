1312490c1aafee703c900216892570cb43f73e4c
maks01vac <maks01vac@gmail.com>
2022-07-13 21:58:20 +0300

    bandlebars_test

---
+++ bandlebars_test/js/index.js
@@ -0,0 +1,7 @@
+var context = { 
+    title: "My New Post", 
+    body: "This is my first post!" 
+};
+var template = Handlebars.compile(document.querySelector("#entry-template").innerHTML)
+var filed = template(context);
+document.querySelector('#outer').innerHTML = filed;
