8880c797892e6efebc7fb436e6feb70332cfb955
maks01vac <maks01vac@gmail.com>
2022-08-03 15:34:18 +0300

    redone with transaction

--- node.js_express_generator/app/store/databaseCourseStore.js
+++ node.js_express_generator/app/store/databaseCourseStore.js
@@ -17,17 +17,30 @@ courseStore.mappingDataFromDatabase = function (data) {
 
 
 courseStore.getAll = async function () {
+    try {
+        const getAllUsersFromData = await db.query('SELECT * FROM users u INNER JOIN username n ON u.id_user=n.id_user')
+        return this.mappingDataFromDatabase(getAllUsersFromData.rows)
+    }
+    catch (err) {
+        throw err
+    }
 
-    const getAllUsersFromData = await db.query('SELECT * FROM users u INNER JOIN username n ON u.id_user=n.id_user')
-    return this.mappingDataFromDatabase(getAllUsersFromData.rows)
 }
 
 courseStore.getById = async function (id) {
-    const getUserById = await db.query('SELECT * FROM users u INNER JOIN username n ON u.id_user=n.id_user WHERE u.id_user=$1', [id])
+    try {
+        const getUserById = await db.query('SELECT * FROM users u INNER JOIN username n ON u.id_user=n.id_user WHERE u.id_user=$1', [id]);
 
-    if (getUserById.rows.length !== 0) {
-        return this.mappingDataFromDatabase(getUserById.rows)
-    } else return false
+        if (getUserById.rows.length !== 0) {
+            return this.mappingDataFromDatabase(getUserById.rows)
+        } else return false
+    }
+    catch (err) {
+        throw err
+    }
+
+
+
 }
 
 courseStore.postCourse = async function (courseData) {
@@ -36,67 +49,68 @@ courseStore.postCourse = async function (courseData) {
     const { firstname, lastname } = name;
 
     try {
-        await db.query('INSERT INTO users (firstname,lastname) VALUES ($1,$2)', [firstname, lastname],);
+        await db.query('BEGIN;')
+
+        const queryInsertInUsers = 'INSERT INTO users(firstname, lastname) VALUES ($1, $2);';
+        await db.query(queryInsertInUsers, [firstname, lastname]);
+
+        const queryInsertInUsername = 'INSERT INTO username SELECT id_user, $1 FROM users WHERE id_user = (SELECT MAX(id_user) FROM users);';
+        await db.query(queryInsertInUsername, [username]);
+
+        await db.query('COMMIT;');
     }
     catch (err) {
-        console.log(err);
-        return false
+        await db.query('ROLLBACK;');
+        throw err
     }
-
-    const queryMaxIdUser = await db.query('SELECT id_user FROM users WHERE id_user = (SELECT MAX(id_user) FROM users)');
-    const idUsername = queryMaxIdUser.rows[0].id_user
-
-    try {
-        await db.query('INSERT INTO username (id_user,username) VALUES ($1,$2)', [idUsername, username])
-    }
-    catch (err) {
-        console.log(err);
-        return false
-    }
-
 }
 
 
-
-courseStore.putById = async function(dataReq,id){
+courseStore.putById = async function (dataReq, id) {
     const { name, username } = dataReq;
     const { firstname, lastname } = name;
 
-
-    const findingId = await db.query('SELECT id_user FROM users WHERE id_user=$1', [id])
-
-    if (findingId.rows.length === 0) {
-        return false
-    }
-
     try {
-        await db.query('UPDATE users SET firstname = $1, lastname =$2 WHERE id_user=$3', [firstname,lastname,id])
-        await db.query('UPDATE username SET username = $1 WHERE id_user=$2', [username,id])
+        await db.query('BEGIN;')
+        const findingId = await db.query('SELECT id_user FROM users WHERE id_user=$1', [id])
+
+        if (findingId.rows.length === 0) return false
+
+        await db.query('UPDATE users SET firstname = $1, lastname =$2 WHERE id_user=$3', [firstname, lastname, id])
+        await db.query('UPDATE username SET username = $1 WHERE id_user=$2', [username, id])
+
+        await db.query('COMMIT;')
         return true
+
     }
     catch (err) {
+        await db.query('ROLLBACK;');
         console.log(err)
         return false
     }
 
 
-  }
+}
 
 
 
 
 courseStore.deleteById = async function (id) {
-    const findingId = await db.query('SELECT id_user FROM users WHERE id_user=$1', [id])
-
-    if (findingId.rows.length === 0) {
-        return false
-    }
 
     try {
-        const deletionRequest = await db.query('DELETE FROM users WHERE id_user=$1', [id])
+        await db.query('BEGIN;')
+
+        const findingId = await db.query('SELECT id_user FROM users WHERE id_user=$1', [id])
+
+        if (findingId.rows.length === 0) return false
+
+        await db.query('DELETE FROM users WHERE id_user=$1', [id])
+
+        await db.query('COMMIT;')
         return true
     }
     catch (err) {
+        await db.query('ROLLBACK;');
         console.log(err)
         return false
     }
